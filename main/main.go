3bed0b70880e838a777c0afb672a735f26012f77
ruitan <tanrui106@163.com>
2019-09-18 17:43:13 +0800

    change db settings to const values

--- main/main.go
+++ main/main.go
@@ -8,13 +8,18 @@ import (
 )
 
 func main() {
+
 	var addr string
 	var mode string
+
+	const dbUrl = "127.0.0.1"
+	const dbSchema = "gotest"
+
 	flag.StringVar(&addr, "addr", ":1323", "server listens at this addr")
 	flag.StringVar(&mode, "mode", "dev", "set server mode")
 	flag.Parse()
 
-	err := db.InitializeGlobalDB("127.0.0.1", "gotest")
+	err := db.InitializeGlobalDB(dbUrl, dbSchema)
 	if err != nil {
 		log.Panic(err)
 	}
